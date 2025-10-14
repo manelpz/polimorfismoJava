fbd66b1814eaa0a15d639aaed5a73343acac6152
Emmanuel Lopez Guerrero <manelpz@icloud.com>
2025-10-14 14:13:45 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// Interfaz común (contrato)
+// interfaz común (contrato)
 interface canalComunicacion {
     void enviarMensaje(String destinatario, String mensaje);
 }
 
-// Implementación: WhatsApp
+// implementación: WhatsApp
 class WhatsApp implements canalComunicacion {
     @Override
     public void enviarMensaje(String destinatario, String mensaje) {
-        System.out.println("📱 Enviando WhatsApp a " + destinatario + ": \"" + mensaje + "\"");
+        System.out.println("enviando whatsApp a " + destinatario + ": \"" + mensaje + "\"");
     }
 }
 
@@ -26,19 +26,19 @@ class WhatsApp implements canalComunicacion {
 // TODO: implementación adicional: Notificación Push
 
 
-// Clase principal que usa polimorfismo
+// clase principal que usa polimorfismo
 public class Main {
     public static void main(String[] args) {
         List<canalComunicacion> medios = new ArrayList<>();
 
-        // Agregamos diferentes medios de comunicación
+        // agregamos diferentes medios de comunicación
         medios.add(new WhatsApp());
         //medios.add(new SMS());
         //medios.add(new ChatIA());
         //medios.add(new Email());
         //medios.add(new NotificacionPush());
 
-        // Simulamos enviar un mensaje por cada medio
+        // simulamos enviar un mensaje por cada medio
 
         canalComunicacion WhatsAppObjeto = medios.get(0);
         WhatsAppObjeto.enviarMensaje("44 31 71 08 10", "Gracias por tu compra emma!");
